881dc2bba6cacda92b4aa7fef42ff1fa1842a028
Ogawa <3223mq118@gmail.com>
2025-06-07 10:46:01 +0900

    a

---
+++ molsim-v1.0/src/integrator.h
@@ -0,0 +1,15 @@
+/*****************************************************************
+ *  Filename     : integrator.h
+ *  Description  : to calcration time step
+ *  Author       : Ogawa <ocmikanmgmg@gmail.com>
+ *  Created      : Jun 7, 2025
+ *  Compiler     : arm-none-eabi-gcc
+ *  SBC          : Raspberry Pi Zero 2 W
+ *  License      : MIT License
+ *  Note         : 
+ *  History      : 2025/06/07 - ファイル作成
+ *****************************************************************/
+#ifndef INTEGRATOR_H
+#define INTEGRATOR_H
+
+#endif
