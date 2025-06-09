b54894ca9e739bffbe4694722b8a3d1c1bb3b646
Ogawa <3223mq118@gmail.com>
2025-06-10 06:27:32 +0900

    output

---
+++ molsim-v3.0/src/output.h
@@ -0,0 +1,12 @@
+/*****************************************************************
+ *  Filename     : output.h
+ *  Description  : run
+ *  Author       : Ogawa <ocmikanmgmg@gmail.com>
+ *  Created      : Jun 7, 2025
+ *  Compiler     : arm-none-eabi-gcc
+ *  SBC          : Raspberry Pi Zero 2 W (32bit)
+ *  License      : MIT License
+ *  Note         : 
+ *  History      : 2025/06/07 - ファイル作成
+ *  最終編集 2025/6/8
+ *****************************************************************/
