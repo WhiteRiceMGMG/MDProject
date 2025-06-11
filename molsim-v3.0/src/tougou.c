7dbf77b27549e241ec7492953c5698d670e30891
WhiteRiceMGMG <3223mq118@gmail.com>
2025-06-12 00:30:54 +0900

    a

--- molsim-v3.0/src/tougou.c
+++ molsim-v3.0/src/tougou.c
@@ -1,3 +1,14 @@
+/*****************************************************************
+ *  Filename     : main.c
+ *  Description  : ファイル分割を統合
+ *  Author       : Ogawa <ocmikanmgmg@gmail.com>
+ *  Created      : Jun 11, 2025
+ *  Compiler     : arm-none-eabi-gcc
+ *  SBC          : Raspberry Pi Zero 2 W
+ *  License      : Ogawa license
+ *  Note         : ファイル全部統合した
+ *  History      : 2025/06/010 - プログラム作成開始
+ *****************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
