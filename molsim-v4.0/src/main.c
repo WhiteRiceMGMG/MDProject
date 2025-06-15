5426988357353ab5758866df5df99830bfc06990
WhiteRiceMGMG <3223mq118@gmail.com>
2025-06-15 11:59:25 +0900

    a

--- molsim-v4.0/src/main.c
+++ molsim-v4.0/src/main.c
@@ -39,7 +39,7 @@ int main(void) {
     parameter->atomInterval       = 0;    /*初期原子間距離*/
     parameter->initialTemperature = 0;    /*初期温度*/
     parameter->currentTemperature = 0;    /*現在の温度*/
-    parameter->boltzmannVal       = 0;    /*ボルツマン定数*/
+    parameter->boltzmannVal       = 1;    /*ボルツマン定数*/
 
     system_message(parameter);
     
