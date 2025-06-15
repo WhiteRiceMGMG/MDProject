be5850fa3c34022b467da40889edba205265f37a
WhiteRiceMGMG <3223mq118@gmail.com>
2025-06-15 17:50:30 +0900

    a

--- molsim-v4.0/src/temperature.c
+++ molsim-v4.0/src/temperature.c
@@ -14,23 +14,28 @@
 
 #define THREE_DIMENSION 3
 
-void control_thermostat(Atom *atom, Parameter *parameter){
-    f8 *atomVelocity        = atom->atomVelocity;    
-    u4 atomNum              = parameter->atomNum;
-    f8 atomMass             = parameter->atomMass;
-    f8 boltzmannVal         = parameter->boltzmannVal;
-    u4 dimension            = 0;
-    f8 temporaryTemperature = 0;
-    u4 i                    = 0;
-
+void calculate_temperature(Atom *atom, Parameter *parameter){
+    f8 *atomVelocity          = atom->atomVelocity;    
+    u4 atomNum                = parameter->atomNum;
+    f8 atomMass               = parameter->atomMass;
+    f8 boltzmannVal           = parameter->boltzmannVal;
+    f8 currentTemperature     = parameter->currentTemperature;
+    f8 initialTemperature     = parameter->initialTemperature;
+    u4 dimension              = 0;
+    f8 temporaryTemperature   = 0;
+    u4 i                      = 0;
+    f8 temperatureCoefficient = 0;
 
     for (i = 0; i < atomNum; i++) {
         for(dimension = 0; dimension < THREE_DIMENSION; dimension++) {
-            temporaryTemperature += atomMass * atomVelocity[dimension] * atomVelocity[dimension]; 
+            temperatureCoefficient = initialTemperature / currentTemperature;
+            temperatureCoefficient *= temperatureCoefficient;
+            temporaryTemperature += atomMass * atomVelocity[dimension] * atomVelocity[dimension]
+                                     * temperatureCoefficient; 
         }
     }
-    parameter->currentTemperature = temporaryTemperature / 3 * atomNum * boltzmannVal;
-    
+    parameter->currentTemperature = temporaryTemperature  / 3 * atomNum * boltzmannVal;
+
 
     printf("hello,world");
 }
