c05e945b24b1557b8ed36d9b77b6d5ec95784b69
ogawa-vaio <3223mq118@gmail.com>
2025-08-24 21:44:05 +0900

    a

--- molsim-v6.0/includes/atom.h
+++ molsim-v6.0/includes/atom.h
@@ -11,4 +11,18 @@
  *  最終編集 2025/08/24
  *****************************************************************/
  #include <includes.h>
- 
+
+ #ifndef ATOM_H
+ #define ATOM_H
+
+ typedef struct
+ {
+    u4 atmId;
+    f8 atmPos[3];
+    f8 atmVel[3];
+    f8 atmFoc[3];
+ } Atom;
+
+ void initialize_atom(Atom  *atom, Parameter *parameter);
+
+ #endif
