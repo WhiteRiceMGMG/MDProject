b1b4cb05282c69d7f25a2af608ebdb22a5919151
ogawa-vaio <3223mq118@gmail.com>
2025-08-24 21:38:23 +0900

    a

---
+++ molsim-v6.0/includes/atom.h
@@ -0,0 +1,13 @@
+/*****************************************************************
+ *  Filename     : atom.h
+ *  Description  : define atom
+ *  Author       : Ogawa <ocmikanmgmg@gmail.com>
+ *  Created      : Aug 24, 2025
+ *  Compiler     : gcc
+ *  SBC          : Raspberry Pi Zero 2 W (32bit)
+ *  License      : OGW License
+ *  Note         : no windows
+ *  History      : 2025/08/24 - ファイル作成
+ *  最終編集 2025/08/24
+ *****************************************************************/
+ 
