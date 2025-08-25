f5bb8d03766d5e2840b464f49602171159cc6ce4
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-25 22:30:36 +0900

    a

---
+++ molsim-v6.0/includes/initAtom.h
@@ -0,0 +1,20 @@
+/*****************************************************************
+ *  Filename     : initAtom.h
+ *  Description  : initial atom  file
+ *  Author       : Ogawa <ocmikanmgmg@gmail.com>
+ *  Created      : Aug 25, 2025
+ *  Compiler     : gcc
+ *  SBC          : Raspberry Pi Zero 2 W (32bit)
+ *  License      : OGW License
+ *  Note         : no windows
+ *  History      : 2025/08/25 - ファイル作成
+ *  最終編集 2025/08/25
+ *****************************************************************/
+#ifndef INITIALATOM_H
+#define INITIALATOM_H
+
+#include "includes.h"
+
+void initial_atom_pos_lattice(Atom *atom, Parameter *param);
+
+#endif
